8b177f5ce9506f539145108c8ada479ed15b1e7a
Michael Ganesan <cookieganesan@gmail.com>
2018-02-19 13:04:07 -0800

    TestCode

--- AngularPrimeNG/testCode/testCode.js
+++ AngularPrimeNG/testCode/testCode.js
@@ -4,4 +4,68 @@ Author:Michael Ganesan
 Date: 02/19/2018
 Objective: Test Code
 Note:All of this code is just test code for nodejs.
-*/ 
+*/
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Colors;
+(function (Colors) {
+    Colors[Colors["Red"] = 0] = "Red";
+    Colors[Colors["Yellow"] = 1] = "Yellow";
+    Colors[Colors["Blue"] = 2] = "Blue";
+    Colors[Colors["Orange"] = 3] = "Orange";
+    Colors[Colors["None"] = 4] = "None";
+})(Colors || (Colors = {}));
+;
+var Dog = /** @class */ (function () {
+    function Dog(coatColor) {
+        this.coatColor = coatColor;
+    }
+    ;
+    Dog.prototype.noise = function (noiseType) {
+        this.bark = noiseType;
+    };
+    Dog.prototype.getNoiseType = function () {
+        console.log("Your noise type is: " + this.bark);
+    };
+    Dog.prototype.getCoatColor = function () {
+        return (Colors[this.coatColor]);
+    };
+    return Dog;
+}());
+var SheepDog = /** @class */ (function (_super) {
+    __extends(SheepDog, _super);
+    function SheepDog(coatColor) {
+        return _super.call(this, coatColor) || this;
+    }
+    return SheepDog;
+}(Dog));
+var tempSheepDog = new SheepDog(Colors.Red);
+tempSheepDog.name = "Coyote";
+tempSheepDog.noise("barking");
+tempSheepDog.getNoiseType();
+console.log("Your dog's coat color is " + tempSheepDog.getCoatColor());
+//Comment: This is a definiton for the function 
+var printName;
+printName = function (name) {
+    console.log("Your name is " + name);
+};
+printName("Michael Ganesan");
+//Optional parameter
+var printMessage = function (message, message2) {
+    if (message2) {
+        console.log("Your message2 is: " + message2);
+    }
+    if (message) {
+        console.log("Your message is: " + message);
+    }
+};
+printMessage("Michael");
+printMessage("Wow this is really cool!", "Michael");
--- AngularPrimeNG/testCode/testCode.ts
+++ AngularPrimeNG/testCode/testCode.ts
@@ -1,4 +1,4 @@
- 
+
 /*
 Filename: TestCode
 Author:Michael Ganesan
@@ -6,3 +6,100 @@ Date: 02/19/2018
 Objective: Test Code
 Note:All of this code is just test code for nodejs. 
 */
+
+enum Colors{
+  Red,
+  Yellow,
+  Blue,
+  Orange,
+  None
+};
+
+
+interface Animal {
+    name: string;
+    noise(noiseType: string): void;
+    getNoiseType(): void;
+    coatColor: Colors;
+    getCoatColor():string;
+
+}
+
+class Dog implements Animal {
+
+    name: string;
+    bark: string;
+    coatColor:Colors; 
+
+
+
+    constructor(coatColor:Colors) {
+       this.coatColor=coatColor;
+
+     };
+
+    noise(noiseType: string) {
+
+        this.bark = noiseType;
+    }
+
+    getNoiseType(): void {
+
+        console.log(`Your noise type is: ${this.bark}`);
+    }
+
+    getCoatColor():string{
+
+         return(Colors[this.coatColor]);
+
+    }
+
+}
+
+class SheepDog extends Dog {
+
+    constructor(coatColor:Colors) {
+        super(coatColor);
+    }
+
+
+}
+
+let tempSheepDog = new SheepDog(Colors.Red);
+tempSheepDog.name = "Coyote";
+tempSheepDog.noise("barking");
+tempSheepDog.getNoiseType();
+console.log(`Your dog's coat color is ${tempSheepDog.getCoatColor()}`);
+
+
+//Comment: This is a definiton for the function 
+var printName: (name:string)=>void;
+
+printName = (name:string)=>{
+   console.log(`Your name is ${name}`);
+};
+
+printName("Michael Ganesan");
+
+//Optional parameter
+var printMessage: (message:string,message2?:string)=>void = (message,message2)=>{
+
+  if(message2){
+
+      console.log(`Your message2 is: ${message2}`);
+
+  }
+
+  if(message){
+
+     console.log(`Your message is: ${message}`);
+
+  }
+
+
+}
+
+printMessage("Michael");
+
+printMessage("Wow this is really cool!","Michael");
+ 
